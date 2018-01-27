f5e9e147342d18ccafd868b014cc8dd971b4dc6a
swhah <hsw0311@nate.com>
2018-01-27 10:29:03 +0900

    1

--- numpytest1.py
+++ numpytest1.py
@@ -7,6 +7,24 @@ print(a.shape)
 print(type(a))
 print(a.dtype)
 
-a.mean(a)
+#a.mean(a)
 
-# 파이썬만으로 합계, 평균, 중위값(위치상 중간), 1/4 
+print('합계:', sum(list1))
+print('평균:', sum(list1)/len(list1))
+
+# 파이썬만으로 합계, 평균, 중위값(위치상 중간)
+list1 = [9, 11, 12, 15, 4, 2, 3, 7, 9, 29]
+
+print('합계:', sum(list1))
+print('평균:', sum(list1)/len(list1))
+
+list1.sort()
+
+if len(list1)%2 == 0:
+    pos = len(list1)//2
+    mean = (list1[pos] + list1[pos+1])/2
+else :
+    pos = len(list1)//2
+    mean = list1[pos]
+
+print('중위수 : ', mean)    
